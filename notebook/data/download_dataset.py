e60209eb1268d2b69d50390871c6010ca5c4dd79
Navneet Tiwari <navneettiwari334@gmail.com>
2024-11-04 08:26:01 +0530

    updated files

---
+++ notebook/data/download_dataset.py
@@ -0,0 +1,17 @@
+import kagglehub
+import shutil
+import os
+
+# Download the dataset
+default_path = kagglehub.dataset_download("lainguyn123/student-performance-factors")
+
+# Define your desired download location
+custom_path = r"C:\ML Projects\mlproject\notebook\data"
+
+# Move all files from default_path to custom_path
+if os.path.exists(default_path):
+    for file_name in os.listdir(default_path):
+        shutil.move(os.path.join(default_path, file_name), custom_path)
+    print(f"Files moved to {custom_path}")
+else:
+    print("Download path does not exist.")
